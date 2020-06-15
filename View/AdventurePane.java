f76a70cf6d500972f0bc0d6bf18328f536a19c46
Panda <2224251265@qq.com>
2020-06-15 09:56:25 +0800

    Develop Plan Update

--- View/AdventurePane.java
+++ View/AdventurePane.java
@@ -8,6 +8,77 @@ import javax.swing.text.PlainDocument;
 
 import java.util.concurrent.locks.*;
 
+class PlantGroup extends JLabel implements Runnable
+{
+    private int x, y;
+    private Thread before;
+    private JButton startButton = new JButton();
+    private final ImageIcon plantGroupIcon = new ImageIcon("./img/Screen/PlantGroupIcon.png");
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        g.drawImage(PlantBarIcon.getImage(), x, y, this);
+    }
+    public PlantChoose(Thread before)
+    {
+        x = 0; y = 600;
+        this.before = before;
+        setBounds(0, 600 - plantGroupIcon.getIconHeight(), plantGroupIcon.getIconWidth(), plantGroupIcon.getIconHeight());
+        setVisible(true);
+    }
+    public void run()
+    {
+        try{
+            if(before != null) before.join();
+            int time = 1000;
+            double ny = y, v = -(double)plantGroupIcon.getIconHeight() / 100;
+            while(time)
+            {
+                time -= 10;
+                Thread.sleep(10);
+                ny += v;
+                y = (int)ny;
+                repaint();
+            }
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class PlantBar extends JLabel implements Runnable
+{
+    final ImageIcon plantBarIcon = new ImageIcon("./img/Screen/PlantBarIcon.png");
+    int x, y;
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        g.drawImage(plantBarIcon.getImage(), x, y, this);
+    }
+    public PlantBar(Thread before){
+
+    }
+    public void run(){
+        try{
+            if(before != null) before.join();
+            int time = 1000;
+            double ny = y, v = -(double)plantBarIcon.getIconHeight() / 100;
+            while(time)
+            {
+                time -= 10;
+                Thread.sleep(10);
+                ny += v;
+                y = (int)ny;
+                repaint();
+            }
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
+
 public class AdventurePane extends JLayeredPane implements Runnable
 {
     final ImageIcon BGImg = new ImageIcon("./img/Background/Background_0.jpg");
