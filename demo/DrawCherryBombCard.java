47a49ddde24f1c08226ffb58df3ebbd1597ce446
yw_c <2553755963@qq.com>
2020-06-20 11:24:38 +0800

    Update DrawCherryBombCard.java
    Cardtest

--- demo/DrawCherryBombCard.java
+++ demo/DrawCherryBombCard.java
@@ -4,40 +4,12 @@ import java.awt.Graphics;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-public class DrawCherryBombCard extends JLabel implements Runnable{
-	int idx = 2;
-	int x ;
-	int y ;
-	int width = 70;
-	int height = 90;
-	public DrawCherryBombCard() {
-		 setBounds(0, 0, 100, 100);
-	     setVisible(true);
-	}
-	
-	@Override
-    public void paint(Graphics g) {
-        super.paint(g);
-        g.drawImage(new ImageIcon("Cards/CherryBomb" + idx + ".png").getImage(),560 , 0, width, height, this);
-    }
-	
-	@Override
-	public void run() {
-		
-		while(true) {
-			if(idx >= 0) {
-				repaint();
-				try {
-					//Draw a picture in 5 seconds
-					Thread.sleep(5000);
-					idx--;
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			else {
-				idx = 2;
-			}
-		}
+public class DrawCherryBombCard extends Card{
+	Image LightIcon = new ImageIcon("Card/CherryBomb0.png");
+	Image DarkIcon = new ImageIcon("Card/CherryBomb2.png");
+	Image FillIcon = new ImageIcon("Card/CherryBomb1.png");
+	Image CursorIcon = new ImageIcon("CherryBomb/boom/CherryBomb_0.png");
+	public DrawCherryBombCard(AdventurePane pane) {
+		super(pane, 0, 72, 15, LightIcon, DarkIcon, FillIcon, CursorIcon);
 	}
 }
